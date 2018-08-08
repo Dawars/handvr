a774314b627a9d140b7c6621230544f6935b0771
Dávid Komorowicz <dawars00@gmail.com>
2018-08-08 09:05:07 +0000

    Mano viewer saves posed obj for every scan registration

--- mano_viewer.py
+++ mano_viewer.py
@@ -9,31 +9,35 @@ print(tf.__version__)
 with open('mpi/data/mano/MANO_RIGHT_py3.pkl', 'rb') as f:
     mano_data = pickle.load(f, encoding='latin1')
 
+batch_size = mano_data['hands_coeffs'].shape[0]
+
 with tf.Session() as sess:
     # init MANO
     mano = SMPL(mano_data)
 
+    hands_components = tf.Variable(mano_data['hands_coeffs'], dtype=tf.float32)
+    hands_coeffs = tf.Variable(mano_data['hands_components'], dtype=tf.float32)
+
+    hands_poses = tf.matmul(hands_components, hands_coeffs)
+
     # cam = N x 3, pose N x self.num_theta, shape: N x 10
-    # cams = theta_here[:, :self.num_cam]
-    poses = tf.Variable(tf.zeros([1, 45 + 3]))
-    shapes = tf.Variable(tf.zeros([1, 10]))
+    cams = tf.tile(tf.zeros([1, 3], dtype=tf.float32), [1554, 1])
+    poses = tf.Variable(tf.concat([cams, mano_data['hands_mean'] + hands_poses], axis=1))
+    shapes = tf.Variable(tf.zeros([batch_size, 10]))
 
     # init vars
     sess.run(tf.global_variables_initializer())
 
-    print(sess.run(mano.v_template))
-
     verts, joints, Rs = mano(shapes, poses, get_skin=True)
 
     verts = sess.run(verts)
+for i, model in enumerate(verts):
+    ## Write to an .obj file
+    with open("./mano_objs/mano_{num}.obj".format(num=i), 'w') as fp:
+        for v in model * 10.:
+            fp.write('v %f %f %f\n' % (v[0], v[1], v[2]))
 
-## Write to an .obj file
-outmesh_path = './MANO.obj'
-with open(outmesh_path, 'w') as fp:
-    for v in verts[0]:
-        fp.write('v %f %f %f\n' % (v[0], v[1], v[2]))
-
-    for f in mano_data['f'] + 1:  # Faces are 1-based, not 0-based in obj files
-        fp.write('f %d %d %d\n' % (f[0], f[1], f[2]))
+        for f in mano_data['f'] + 1:  # Faces are 1-based, not 0-based in obj files
+            fp.write('f %d %d %d\n' % (f[0], f[1], f[2]))
 
 print('done')
