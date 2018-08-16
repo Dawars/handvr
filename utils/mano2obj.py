66796c783fa602d2bfe3c5ea7f4ab7905b727e5e
Vince Szabó <cakewithsteak@gmail.com>
2018-08-16 17:29:46 +0200

    Added a function to write mano results to obj files

--- utils/mano2obj.py
+++ utils/mano2obj.py
@@ -1,13 +1,29 @@
 import json
 import tensorflow as tf
-from hmr.src.tf_smpl.batch_smpl import SMPL
+from tf_smpl.batch_smpl import SMPL
+import pickle
 
-with open('/datasets/handvr/mano/hands_components.json') as f:
-    hands_components = json.load(f)
-with open('/datasets/handvr/mano/hands_coeffs.json') as f:
-    hands_coeffs = json.load(f)
+
+#with open('/datasets/handvr/mano/hands_components.json') as f:
+#    hands_components = json.load(f)
+#with open('/datasets/handvr/mano/hands_coeffs.json') as f:
+#    hands_coeffs = json.load(f)
+
+
+with open('mpi/data/mano/MANO_RIGHT_py3.pkl', 'rb') as f:
+    mano_data = pickle.load(f, encoding='latin1')
 
 mano = SMPL('/projects/handvr/mpi/data/mano/mano_params.pkl')
 
 
-print('end')
+def manoToOBJ(shapes, poses, filename):
+    vertices = mano(shapes, poses, get_skin=True)
+    model = vertices[0]
+
+    with open(filename, 'w') as fp:
+        for v in model * 10.:
+            fp.write('v %f %f %f\n' % (v[0], v[1], v[2]))
+
+        for f in mano_data['f'] + 1:  # Faces are 1-based, not 0-based in obj files
+            fp.write('f %d %d %d\n' % (f[0], f[1], f[2]))
+
