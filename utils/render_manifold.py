f69fac07730c93885a09553eb5517a7490bd4c4a
Dávid Komorowicz <dawars00@gmail.com>
2018-08-30 10:44:12 +0000

    Fine tune rendering

--- utils/render_manifold.py
+++ utils/render_manifold.py
@@ -25,37 +25,50 @@ fragment_shader = '''
                    in vec3 out_normal;
                    out vec4 f_color;
 
-                   const vec3 lightDir = vec3(1, -1, 1);
-                   void main() {
-                       vec3 light = -normalize(lightDir);
-
+                   const vec3 light1 = vec3(1, 1, -1);
+                   const vec3 light2 = vec3(0, 0, 1);
+                   
+                   const vec3 color = vec3(1, 0, 0);
+                   const vec3 ambientLight = vec3(0.13, 0.13, 0.13);
+                  const float shininess = 16.0;
+                     void main() {
+                        vec3 viewDir = vec3(0,0,1);
                        vec3 normal = normalize(out_normal);
-                       float lambert = max(0, dot(normal, light));
+                       float lambert1 = max(0, dot(normal, -normalize(light1)));
+                       float lambert2 = max(0, dot(normal, -normalize(light2)));
 
-                       vec3 color = lambert * vec3(1.0, 0.0, 0.0);
-                       f_color = vec4(normal, 1.0);
+                    vec3 halfDir = normalize(light2 + viewDir);
+                        float specAngle = max(dot(halfDir, normal), 0.0);
+                        float specular = pow(specAngle, shininess);
+                        
+
+                        vec3 final_color = ambientLight + 
+                                             0.8 * lambert1 * color + 
+                                             1.0 * lambert2 * color;
+
+                       f_color = vec4(final_color, 1.0);
                    }
                    '''
 geometry_shader = '''
-#version 330 core
-
-layout(triangles) in;
-layout(triangle_strip, max_vertices = 3) out;
-
-out vec3 out_normal;
-
-void main() {
-    vec4 edge1 = gl_in[0].gl_Position - gl_in[1].gl_Position;
-    vec4 edge2 = gl_in[0].gl_Position - gl_in[2].gl_Position;
-    out_normal = cross(edge1.xyz, edge2.xyz);
-    
-    for(int i = 0; i < 3; i++) {
-        gl_Position = gl_in[i].gl_Position;
-        EmitVertex();
-    }
-    EndPrimitive();
-}
-'''
+                #version 330 core
+                
+                layout(triangles) in;
+                layout(triangle_strip, max_vertices = 3) out;
+                
+                out vec3 out_normal;
+                
+                void main() {
+                    vec4 edge1 = gl_in[0].gl_Position - gl_in[1].gl_Position;
+                    vec4 edge2 = gl_in[0].gl_Position - gl_in[2].gl_Position;
+                    out_normal = cross(edge1.xyz, edge2.xyz);
+                    
+                    for(int i = 0; i < 3; i++) {
+                        gl_Position = gl_in[i].gl_Position;
+                        EmitVertex();
+                    }
+                    EndPrimitive();
+                }
+                '''
 
 num_vertices = 778
 
@@ -94,8 +107,8 @@ class HandRenderer:
 
         # Framebuffers
 
-        self.fbo1 = self.ctx.framebuffer(self.ctx.renderbuffer((image_size, image_size), samples=4))
-        self.fbo2 = self.ctx.framebuffer(self.ctx.renderbuffer((image_size, image_size)))
+        self.fbo1 = self.ctx.framebuffer([self.ctx.renderbuffer((image_size, image_size), samples=8)])
+        self.fbo2 = self.ctx.framebuffer([self.ctx.renderbuffer((image_size, image_size))])
 
     def __del__(self):
 
@@ -106,7 +119,10 @@ class HandRenderer:
         self.fbo1.release()
         self.fbo2.release()
 
-    def render_manifold(self, decoder, name="./manifold.png", bounds=(-4, 4), steps=0.5, verbose=False):
+    def render_manifolds(self, decoder, name="./manifold.png", bounds=(-4, 4), steps=0.5, verbose=False):
+        pass
+
+    def render_manifold(self, decoder, name="./manifold.png", bounds=(-4, 4), num_samples=16, verbose=False):
         """
         Render a 2D posed hand manifold
         :param decoder: pytorch decoder function 2 -> 45 params, should be called with torch.no_grad():
@@ -116,10 +132,11 @@ class HandRenderer:
         :param verbose: print progress
         :returns rendered image
         """
-
         os.makedirs(os.path.dirname(name), exist_ok=True)
 
-        result_length = self.image_size * (bounds[1] - bounds[0]) / steps
+        result_length = self.image_size * num_samples
+
+        steps = (bounds[1] - bounds[0]) / num_samples
 
         # coordinates to sample at
         sampling_grid = np.mgrid[bounds[0]:bounds[1]:steps, bounds[0]:bounds[1]:steps]
@@ -131,6 +148,8 @@ class HandRenderer:
         batch_size = len(encoded)
 
         rot = np.zeros([batch_size, 3])
+        rot[:, 0] = np.pi / 4
+
         shape = np.zeros([batch_size, 10])
 
         decoded_poses = decoder(encoded)
@@ -145,15 +164,15 @@ class HandRenderer:
                 if verbose:
                     print("Rendering at {x}, {y}".format(x=x, y=y))
 
-                model_index = y * rows + x
+                model_index = (rows - y - 1) * rows + x
 
                 img = self.render_mano(vertices[model_index])
                 # mano_to_OBJ(shape, decoded_poses, "./test.obj")
 
                 x_pos = x * self.image_size
-                y_pose = y * self.image_size
+                y_pos = y * self.image_size
 
-                res.paste(img, (int(x_pos), int(y_pose)))
+                res.paste(img, (int(x_pos), int(y_pos)))
         if verbose:
             print("Manifold rendered")
         res.save(name)
@@ -166,7 +185,7 @@ class HandRenderer:
         :return image of the hand
         """
         vertices = mano_vertices * 10.
-        vertices[:, 0] -= 0.141
+        vertices[:] -= [0.15, 0.0, 0.0]
 
         self.vboPos.write(vertices.astype('f4').tobytes())
 
@@ -179,46 +198,37 @@ class HandRenderer:
 
         self.ctx.copy_framebuffer(self.fbo2, self.fbo1)
         data = self.fbo2.read(components=3, alignment=1)
-        img = Image.frombytes('RGB', self.fbo2.size, data).transpose(Image.FLIP_TOP_BOTTOM)
+        img = Image.frombytes('RGB', self.fbo2.size, data)  # .transpose(Image.FLIP_TOP_BOTTOM)
 
         # img.show()
         return img
 
 
 if __name__ == '__main__':
-    renderer = HandRenderer(128)
+    renderer = HandRenderer(256)
 
     # rendering mano
-    pose = np.concatenate([np.array([[np.pi / 2, 0, 0]]), np.zeros([1, 45])], axis=1)
+    pose = np.zeros([1, 48])
+    finger = 0
+    joint = 0
+    i = 3 + 3 * finger + joint
+    # pose[0, i + 1] = np.pi / 2
+    pose[0, 0] = np.pi / 2
 
-    start = time.time()
-    # for i in range(100):
+    from block_timer.timer import Timer
+
     poses = get_mano_vertices(np.zeros([1, 10]), pose)
-    end = time.time()
 
-    elapsed = end - start
-    print(elapsed)
-
-    start = time.time()
-    img = renderer.render_mano(poses[0])
-    end = time.time()
-
-    elapsed = end - start
-    print(elapsed)
+    with Timer():
+        img = renderer.render_mano(poses[0])
 
     plt.imsave('rendering_test.png', img)
 
     # rendering manifold
     from pose_autoencoders.vanilla_ae import autoencoder
 
-    ae = autoencoder()  # Load a premade autoencoder
+    ae = autoencoder().cuda()  # Load a premade autoencoder
     ae.load_state_dict(torch.load('../pose_autoencoders/sim_autoencoder.pth'))
 
-    import time
-
-    start = time.time()
-    renderer.render_manifold(ae.decoder, './manifold_test.png', verbose=False)
-    end = time.time()
-
-    elapsed = end - start
-    print(elapsed)
+    with Timer():
+        renderer.render_manifold(ae.decoder, './manifold_test.png', verbose=False)
